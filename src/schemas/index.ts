60b0dd4f437c885426087fab14218c6930317ad4
alashchev17 <andrew.lashchev15@gmail.com>
2024-06-29 12:34:04 +0200

    fix: schemas dependencies fix

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,8 +1,7 @@
 import informationSchema from './information.schema'
 import serviceSchema from './service.schema'
-import questionSchema from './question.schema'
 import questionServicesSchema from './questionServices.schema'
 
-const schemas = [informationSchema, serviceSchema, questionServicesSchema, questionSchema]
+const schemas = [informationSchema, serviceSchema, questionServicesSchema]
 
 export default schemas
