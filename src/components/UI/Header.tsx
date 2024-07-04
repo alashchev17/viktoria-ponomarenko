7c10a5fac29dcdb9ada1ab88a1f91c31d898071c
alashchev17 <andrew.lashchev15@gmail.com>
2024-07-04 18:41:28 +0200

    fix: header z-index fix

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -4,7 +4,7 @@ export const Header = async () => {
   const title = (await getInformation()).name;
 
   return (
-    <header className="fixed top-0 left-0 flex w-full justify-center h-[60px] px-4 py-2 font-serif text-4xl bg-white">
+    <header className="fixed top-0 left-0 z-50 flex w-full justify-center h-[60px] px-4 py-2 font-serif text-4xl bg-white">
       V
     </header>
   );
