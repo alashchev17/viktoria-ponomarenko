9029f04045f89f2fe8720e1323f87026b9811b38
alashchev17 <andrew.lashchev15@gmail.com>
2024-11-17 17:24:11 +0100

    fix: layout adjustment

--- src/components/Slogan/SloganSection.styles.ts
+++ src/components/Slogan/SloganSection.styles.ts
@@ -7,14 +7,7 @@ const fullConfig = getTailwindConfig()
 
 export const Slogan = styled.section`
   position: relative;
-  // background-color: ${fullConfig.theme.backgroundColor['dark-blue'] as string};
-  // background-image: url('/section-bg.png');
-  // background-size: cover;
-  // background-position: center center;
-  // background-repeat: no-repeat;
-  // background-blend-mode: lighten;
   padding: 0 60px;
-  // z-index: -1;
 `
 
 export const Container = styled.div`
@@ -22,21 +15,21 @@ export const Container = styled.div`
   overflow: hidden;
   @media screen and (min-width: 1024px) {
     display: flex;
+    justify-content: center;
     gap: 16px;
     max-width: 1140px;
     margin: 0 auto;
     padding: 120px 0 160px 0;
+    flex-wrap: wrap;
   }
 `
 export const Circle = styled.button`
   position: relative;
   max-width: 358px;
-  margin: 0 auto;
   width: 100%;
   height: auto;
   aspect-ratio: 1 / 1;
   display: flex;
-  // padding: 75.5px 10px 62.5px 10px;
   align-items: center;
   justify-content: center;
   border-radius: 50%;
